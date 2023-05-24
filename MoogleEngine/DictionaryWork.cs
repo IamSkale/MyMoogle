21f51b2bf7aa5db1dfd39e4608341cca16d5d9ed
IamSkale <128943389+IamSkale@users.noreply.github.com>
2023-05-23 22:20:45 -0400

    Add files via upload

---
+++ MoogleEngine/DictionaryWork.cs
@@ -0,0 +1,143 @@
+﻿namespace MoogleEngine;
+public class DictionaryWork
+{   
+    public Dictionary<string, Dictionary <string, int>> fileDictionaries;
+
+    public Dictionary<string, Dictionary <string, List<int>>> indexDictionaries;
+
+    public Dictionary<string, Dictionary<string, double>> tfidfDictionaries;
+
+    public Dictionary<string,List<string>> texts;
+
+    public string [] fileNames;
+
+    public DictionaryWork()
+    {
+        string [] files = Directory.GetFiles(@"..\Content");
+
+        fileNames = new string[files.Length];
+
+        for (int i = 0; i < files.Length; i++)
+        {
+            fileNames[i] = Path.GetFileName(files[i]); 
+        } 
+
+        fileDictionaries = new Dictionary<string, Dictionary<string, int>>();
+
+        indexDictionaries = new Dictionary<string, Dictionary<string, List<int>>>();
+
+        tfidfDictionaries = new Dictionary<string, Dictionary<string, double>>();
+
+        texts = new Dictionary<string, List<string>>();
+
+        FillDictionaries(fileDictionaries,fileNames,indexDictionaries,texts);
+        TfIdf(fileDictionaries,fileNames,tfidfDictionaries);
+    }
+    
+    static void FillDictionaries(Dictionary<string, Dictionary <string, int>> fileDictionaries, string [] fileNames, Dictionary<string, Dictionary <string, List<int>>> indexDictionaries, Dictionary<string,List<string>> texts)
+    {
+        int i = 0;
+        foreach (string fileName in fileNames)
+        {
+            string path = @"..\Content\" + fileName;
+
+            string text2 = "";
+
+            using (StreamReader oSR = File.OpenText(path))
+            {
+                string s = "";
+                while ((s= oSR.ReadLine()) != null)
+                {
+                    text2 += s;   
+                }
+            }
+            var fileWordTf = new Dictionary <string, int>();
+
+            var wordIndex = new Dictionary <string, List<int>>();
+
+            texts[fileName] = new List<string>();
+
+            string tempWord = "";
+
+            int j = 0;
+
+            foreach(char c in text2)
+            {
+                if(Char.IsLetter(c))
+                {
+                    tempWord += Char.ToLower(c);
+                }
+                else if ((c == ' ' || Char.IsPunctuation(c)) && tempWord !="")
+                {
+                    texts[fileName].Add(tempWord);
+                    
+                    if(fileWordTf.ContainsKey(tempWord))
+                    {
+                        fileWordTf[tempWord]++;
+                        wordIndex[tempWord].Add(j);
+                    }
+                    else
+                    {
+                        fileWordTf[tempWord] = 1; 
+                        List<int> indexes = new List<int>();
+                        indexes.Add(j);
+                        wordIndex[tempWord] = indexes;
+                    }
+                    j++;
+                    tempWord = "";
+                } 
+            }
+            fileDictionaries[fileName] = fileWordTf;
+            indexDictionaries[fileName] = wordIndex;
+            i++;
+        }
+    }
+
+    static void TfIdf(Dictionary<string, Dictionary <string, int>> fileDictionaries, string [] fileNames, Dictionary<string, Dictionary <string, double>> tfidfDictionaries)
+    {
+        int docCount = fileNames.Length;
+
+        var documentFrequency = new Dictionary<string, int>();
+
+        foreach (var dict in fileDictionaries.Values)
+        {
+            foreach (string item in dict.Keys)
+            {
+                documentFrequency[item] = 0;
+
+                for (int i = 0; i < fileNames.Length; i++)
+                {
+                    if(fileDictionaries[fileNames[i]].ContainsKey(item))
+                    {
+                        documentFrequency[item]++;
+                    }
+                }
+            }
+        }
+
+        var inverseDocumentFrequency = new Dictionary<string, double>();
+
+        foreach (var term in documentFrequency.Keys)
+        {
+            inverseDocumentFrequency[term] = Math.Log((double)docCount / documentFrequency[term]);
+        }
+
+        foreach (var document in fileDictionaries)
+        {
+            string documentName = document.Key;
+
+            tfidfDictionaries[documentName] = new Dictionary<string, double>();
+
+            foreach (var term in document.Value)
+            {
+                double tf = 1 + Math.Log((double)term.Value);
+
+                double idf = inverseDocumentFrequency[term.Key];
+
+                double tfidf = tf * idf;
+
+                tfidfDictionaries[documentName][term.Key] = tfidf;
+            }
+        }
+    }
+}
---
+++ MoogleEngine/ScoreWork.cs
@@ -0,0 +1,56 @@
+﻿namespace MoogleEngine;
+public class ScoreWork
+{   
+    public static void InsertionSortDescending(double [] arr, string [] arrII) 
+    {
+        for (int i = 1; i < arr.Length; i++) 
+        {
+            int j = i;
+            while (j > 0 && arr[j - 1] < arr[j]) 
+            {
+                double temp = arr[j];
+                string tempII = arrII[j];
+                arr[j] = arr[j - 1];
+                arrII[j] = arrII[j-1];
+                arr[j - 1] = temp;
+                arrII[j-1] = tempII;
+                j--;
+            }
+        }
+    }
+    
+    public static double[] Score(Dictionary<string,Dictionary<string, double>> tfidfDictionaries, string query, string[] fileNames,Dictionary<string, Dictionary <string, int>> fileDictionaries,Dictionary<string, Dictionary <string, List<int>>> indexDictionaries, Dictionary<string,List<string>> texts)
+    {
+        double [] score = new double[fileNames.Length];
+
+        double queryNorm = 0;
+
+        var auxQueryTFIDF = QueryWork.QueryTFIDF(tfidfDictionaries,query,fileNames);
+
+        foreach (double item in auxQueryTFIDF.Values)
+        {
+            queryNorm += Math.Pow(item,2);
+        }
+        for (int i = 0; i < fileNames.Length; i++)
+        {
+            double textNorm = 0;
+            foreach (var item in auxQueryTFIDF)
+            {
+                if(tfidfDictionaries[fileNames[i]].ContainsKey(item.Key))
+                {
+                    score[i] += item.Value * tfidfDictionaries[fileNames[i]][item.Key];
+                    textNorm += Math.Pow(tfidfDictionaries[fileNames[i]][item.Key],2);
+                }
+            }
+            if (textNorm!=0)
+            {
+                score[i] = score[i]/(Math.Sqrt((float)queryNorm)*Math.Sqrt((float)textNorm));
+            }
+            else
+            {
+                score[i] = 0;
+            }
+        }
+        return score;
+    }
+}
